ed2d5b01c212acd7d8d32980f7cf35ad91dc53fc
anchh <anchalsharma31@gmail.com>
2023-01-23 12:20:01 +0530

    feat: basic backend implementation

---
+++ dbapp.py
@@ -0,0 +1,245 @@
+from sqlalchemy import create_engine
+import json
+from flask import Flask
+from flask import request
+from flask import Response
+from sqlalchemy.orm import sessionmaker
+from table import Base, Product
+import requests
+
+db_string = "postgresql://unbxd:myPassword@localhost:5432/catalog"
+
+db = create_engine(db_string)
+
+Base.metadata.create_all(db)
+
+Session = sessionmaker(bind=db)
+
+app = Flask("CatalogEntry")
+#app.run(debug=True)
+
+#Clear the database
+@app.route('/clear', methods = ['POST'])
+def clear():
+    print(request)
+    if request.form['password'] == 'myPassword':
+        try:
+                Base.metadata.drop_all(db)
+                # Ok - table deleted
+                return Response(json.dumps({
+                        'status': 'ok'
+                }), status=200, mimetype='application/json')
+        except Exception as exc:
+                # server error
+                return Response(json.dumps({
+                        'status': 'server_error',
+                        'error': str(exc)
+                }), status=500, mimetype='application/json')
+        
+
+#insert products one at a time into the database
+@app.route('/insert', methods = ['POST'])
+def insert():
+        #print("request",request.json)
+        try:    
+                
+                # Check if the relevant request arguments have been provided
+                if not (
+                        'sku' in request.json and
+                        'title' in request.json and
+                        'productDescription' in request.json and
+                        'price' in request.json and
+                        'productImage' in request.json and
+                        'catlevel1Name' in request.json and
+                        'catlevel2Name' in request.json
+                ):
+                        # Bad request
+                        return Response(json.dumps({
+                                'status': 'bad_request',
+                                'error': 'Missing required arguments'
+                        }), status=400, mimetype='application/json')
+
+        # Add a new product
+                new_product = Product(
+                        sku = request.json['sku'],
+                        title = request.json['title'],
+                        productdescription = request.json['productDescription'],
+                        price = request.json['price'],
+                        productimage = request.json['productImage'],
+                        catlevel1name = request.json['catlevel1Name'],
+                        catlevel2name = request.json['catlevel2Name']
+                )
+
+        # Add the product to the database
+                s = Session()
+                s.add(new_product)
+                s.commit()
+                s.close()
+
+        # Ok - product added
+                return Response(json.dumps({
+                        'status': 'ok'
+                }), status=200, mimetype='application/json')
+
+        except Exception as exc:
+                print(exc)
+                #server error
+                return Response(json.dumps({
+                        'status': 'server_error',
+                        'error': str(exc)
+                }), status=500, mimetype='application/json')
+        
+#get names of categories and sub categories to populate the dropdowns       
+@app.route('/get_category', methods = ['GET'])
+def get_category():
+        #get all the categories
+        try:
+                s = Session()
+                result_temp = s.query(Product).all()
+                #response = []
+                catlevel1 = []
+                catlevel2 = []
+                for row in result_temp:
+                        if row.catlevel1name not in catlevel1:
+                                catlevel1.append(row.catlevel1name)
+                        if row.catlevel2name not in catlevel2:
+                                catlevel2.append(row.catlevel2name)
+                return Response(json.dumps({'catlevel1Name': catlevel1, 'catlevel2Name': catlevel2}), status = 200, mimetype='application/json')
+
+                #return Response(json.dumps({
+                #         'status': 'ok',
+                #         'sku': product.sku,
+                #         'title': product.title,
+                #         'productDescription': product.productdescription,
+                #         'price': product.price,
+                #         'productImage': product.productimage,
+                #         'catlevel1Name': product.catlevel1name,
+                #         'catlevel2Name': product.catlevel2name
+                # }), status=200, mim
+        
+        except Exception as exc:
+                #server error
+                return Response(json.dumps({
+                        'status': 'server_error',
+                        'error': str(exc)
+                }), status=500, mimetype='application/json')
+        
+#Get all the products in a category and sub-category       
+@app.route('/category', methods = ['GET'])
+def category():
+        try:
+                # Check if the relevant request arguments have been provided
+                if not (
+                        'catlevel1Name' in request.args or
+                        'catlevel2Name' in request.args
+                ):
+                        # Bad request
+                        return Response(json.dumps({
+                                'status': 'bad_request',
+                                'error': 'Missing required arguments'
+                        }), status=400, mimetype='application/json')
+        
+                # Get the product from the database
+                catlevel1Name = request.args['catlevel1Name']
+                catlevel2Name = request.args['catlevel2Name']
+                s = Session()
+                result_temp = s.query(Product).filter(Product.catlevel1name == catlevel1Name, Product.catlevel2name==catlevel2Name).all()
+                #response = []
+                temp = []
+                result_count = 0
+                for row in result_temp:
+                        result_count += 1
+                        temp.append(Product.jsonformat(row))
+                
+                return Response(json.dumps({'count': result_count, 'products': temp}), status = 200, mimetype='application/json')
+        
+        except Exception as exc:
+                # Server error
+                return Response(json.dumps({
+                        'status': 'server_error',
+                        'error': str(exc)
+                }), status=500, mimetype='application/json')
+        
+#Get individual product details fetched on sku
+@app.route('/product', methods = ['GET'])
+def get_product():
+       print("here")
+       try:
+                      # Check if the relevant request arguments have been provided
+                        if not (
+                                'sku' in request.args
+                        ):
+                                # Bad request
+                                return Response(json.dumps({
+                                        'status': 'bad_request',
+                                        'error': 'Missing required arguments'
+                                }), status=400, mimetype='application/json')
+        
+                        # Get the product from the database
+                        s = Session()
+                        product = s.query(Product).filter_by(sku = request.args['sku']).first()
+                        s.close()
+        
+                        if product is None:
+                                # Not found
+                                return Response(json.dumps({
+                                        'status': 'not_found'
+                                }), status=404, mimetype='application/json')
+        
+                        # Ok - product found
+                        return Response(json.dumps({
+                                'status': 'ok',
+                                'sku': product.sku,
+                                'title': product.title,
+                                'productDescription': product.productdescription,
+                                'price': product.price,
+                                'productImage': product.productimage,
+                                'catlevel1Name': product.catlevel1name,
+                                'catlevel2Name': product.catlevel2name
+                        }), status=200, mimetype='application/json')    
+        
+       except Exception as exc:
+                       # Server error
+                        return Response(json.dumps({
+                                'status': 'server_error',
+                                'error': str(exc)
+                        }), status=500, mimetype='application/json')
+
+#Get results based on search query         
+@app.route('/search', methods = ['GET'])
+def search():
+        #print("here")
+        try:
+                # Check if the relevant request arguments have been provided
+                if not (
+                        'searchTerm' in request.args
+                ):
+                        # Bad request
+                        return Response(json.dumps({
+                                'status': 'bad_request',
+                                'error': 'Missing required arguments'
+                        }), status=400, mimetype='application/json')
+
+                # Get the search term
+                search_term = request.args['searchTerm']
+                search_term= search_term.replace(" ","+")
+                #print(search_term)
+                # Get the products from the database
+                link = "https://search.unbxd.io/fb853e3332f2645fac9d71dc63e09ec1/demo-unbxd700181503576558/search?q={}".format(search_term)
+                #print(link)
+                response = json.loads(requests.get(link).content)['response']
+                final_response = {'products': [], 'numberOfProducts': 0}
+                for i in response['products']:
+                        final_response['products'].append({'sku': i['sku'], 'title': i['title'], 'productdescription': i['description'], 'price': i['price'], 'productimage': i['productImage'], 'catlevel1name': i['catlevel1Name'], 'catlevel2name': i['catlevel2Name']})
+                final_response['numberOfProducts'] = response['numberOfProducts']
+                return Response(json.dumps(final_response), status=200, mimetype='application/json')
+        
+        except Exception as exc:
+                # Server error
+                return Response(json.dumps({
+                        'status': 'server_error',
+                        'error': str(exc)
+                }), status=500, mimetype='application/json')
+
+if __name__ == '__main__':
+        app.run(debug= True)
---
+++ json_to_db_pipeline.py
@@ -0,0 +1,26 @@
+import json
+import requests
+catalog = json.load(open('out.json'))
+for i in catalog:
+    try:
+        if 'sku' not in i:
+            pass
+        if 'title' not in i:
+            i['title'] = "NA"
+        if 'productDescription' not in i:
+            i['productDescription'] = "NA"
+        if 'price' not in i:
+            i['price'] = 0.0
+        if 'productImage' not in i:
+            i['productImage'] = "NA"
+        if 'catlevel1Name' not in i:
+            i['catlevel1Name'] = "NA"
+        if 'catlevel2Name' not in i:
+            i['catlevel2Name'] = "NA"
+
+        r = requests.post('http://127.0.0.1:5000/insert', json=i)
+        #print(f"Status Code: {r.status_code}, Response: {r.json()}")
+
+    except:
+        print("AHHHHH")
+        break
